b2b4515c9ff61b32445f7f7bb0fe7e4b2b37fda3
Mingyu Choo <mingyuchoo@gmail.com>
2022-07-05 06:24:34 +0900

    updated doc

--- strings/parse.exs
+++ strings/parse.exs
@@ -9,4 +9,23 @@ defmodule Parse do
     do: _number_digits(tail, value * 10 + digit - ?0)
 
   defp _number_digits([non_digit | _], _), do: raise("Invalid digit '#{[non_digit]}'")
+
+  @doc """
+  -- in Haskell
+
+  import           Data.Char (digitToInt)
+
+  number :: [Char] -> Int
+  number ('-':xs) = numberDigits xs  (0  * (-1))
+  number ('+':xs) = numberDigits xs  0
+  number xs       = numberDigits xs  0
+
+  numberDigits :: [Char] -> Int -> Int
+  numberDigits [] v = v
+  numberDigits (x:xs) v | x `elem` "0123456789" = numberDigits xs (v*10 + checkZero x)
+  numberDigits (x:_) _ = error $ "Invalid digit " ++ show x
+
+  checkZero :: Char -> Int
+  checkZero d = if d == '0' then 0 else digitToInt d
+  """
 end
