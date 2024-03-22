a325288a9fae610346c03b499ef29e58fca28aed
mingyuchoo <mingyuchoo@gmail.com>
2024-03-23 06:22:58 +0900

    re-structured

--- basic/03_operations.exs
+++ basic/03_operations.exs
@@ -1,3 +1,6 @@
 IO.puts "Arithmetic -----------------------"
+
+# div/2 -> div(integer(), neg_integer() | pos_integer()) :: integer()
+
 Enum.each([{2, 2}],
   fn (x) -> IO.puts "#{x}" end)
--- basic/04_with_scope.exs
+++ basic/04_with_scope.exs
@@ -1,12 +1,9 @@
-content = "Now is the time"
-
-lp =
+result =
   with {:ok, file} = File.open("/etc/passwd"),
        content = IO.read(file, :all),
        :ok = File.close(file),
-       [_, uid, gid] = Regex.run(~r/^_lp:.*?:(\d+):(=\d+)/m, content) do
+       [_, uid, gid] <- Regex.run(~r/^_lp:.*?:(\d+):(=\d+)/m, content) do
     "Group: #{gid}, User: #{uid}"
   end
 
-IO.puts(lp)
-IO.puts(content)
+IO.puts(inspect(result))
