3375a200ded519358f52cc743bf58a9b0384b071
mingyuchoo <mingyuchoo@gmail.com>
2024-03-30 06:11:53 +0900

    added

--- webs/demo_umbrella/apps/demo_web/lib/demo_web/live/role_live/show.ex
+++ webs/demo_umbrella/apps/demo_web/lib/demo_web/live/role_live/show.ex
@@ -16,6 +16,15 @@ defmodule DemoWeb.RoleLive.Show do
      |> assign(:role, Accounts.get_role!(id))}
   end
 
+  @impl true
+  def handle_event("delete", %{"id" => id}, socket) do
+    role = Accounts.get_role!(id)
+    {:ok, _} = Accounts.delete_role(role)
+
+    # 수정
+    {:noreply, push_navigate(socket, to: "/roles")}
+  end
+
   defp page_title(:show), do: "Show Role"
   defp page_title(:edit), do: "Edit Role"
 end
