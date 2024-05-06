1650c9aab41d46c1ba0ca96d427fc2d33e228947
mingyuchoo <mingyuchoo@gmail.com>
2024-05-07 04:57:57 +0900

    added sequence

---
+++ apps/sequence/README.md
@@ -0,0 +1,12 @@
+# README
+
+## How to use
+
+```bash
+$ mix deps.get
+$ iex -S mix
+
+iex> Sequence.Server.next_number
+iex> Sequence.Server.increment_number 100
+iex> C-c,C-c
+```
---
+++ apps/sequence/lib/sequence/server.ex
@@ -0,0 +1,33 @@
+defmodule Sequence.Server do
+  use GenServer
+
+  @me __MODULE__
+
+  def start_link _ do
+    GenServer.start_link(@me, nil, name: @me)
+  end
+
+  def next_number do
+    GenServer.call @me, :next_number
+  end
+
+  def increment_number delta do
+    GenServer.cast @me, {:increment_number, delta}
+  end
+
+  def init _ do
+    {:ok, Sequence.Stash.get()}
+  end
+
+  def handle_call :next_number, _from, current_number do
+    {:reply, current_number, current_number+1}
+  end
+
+  def handle_cast {:increment_number, delta}, current_number do
+    {:noreply, current_number + delta}
+  end
+
+  def terminate _reason, current_number do
+    Sequence.Stash.update(current_number)
+  end
+end
